1051b715fcf4c50a09c7a46652a5ef269a8280a6
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-05-21 23:11:54 +0900

    RSpec初テスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,9 +30,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+require 'capybara/rspec' # 追加
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f } 
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Devise::Test::IntegrationHelpers, type: :system
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false

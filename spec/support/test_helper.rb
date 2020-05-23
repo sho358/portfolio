d2b7b11005ed22d702d15acd7cecefc321cd0782
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-05-23 09:28:17 +0900

    7-6完了、テストで二つエラー（ユーザー新規登録から、users/showへいかない。）

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -7,6 +7,13 @@ end
 def login_for_request(user)
     post login_path, params: { session: { email: user.email,
                                           password: user.password } }
+end
+
+def login_for_system(user)
+    visit login_path
+    fill_in "user_email",    with: user.email
+    fill_in "user_password", with: user.password
+    click_button "ログイン"
   end
   
   def login_remember(user)

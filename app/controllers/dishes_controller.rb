5160d6b96770cecd183d17e1bae0d13cf5337240
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-05-24 11:37:37 +0900

    8-4 エラーが3つある

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -5,4 +5,21 @@ class DishesController < ApplicationController
     @dish = Dish.new
  end
 
+ def create
+    @dish = current_user.dishes.build(dish_params)
+    if @dish.save
+      flash[:success] = "料理が登録されました！"
+      redirect_to root_url
+    else
+      render 'dishes/new'
+    end
+  end
+
+ private
+
+ def dish_params
+   params.require(:dish).permit(:name, :discription, :portion, :tips,
+                                :reference, :required_time, :popularity, :cook_memo)
+ end
+
 end

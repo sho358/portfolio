e6e8b9f1fc967cc01c49a173d68a17e32dd617c8
sho358 <ishikawa.hokudai.2013@gmail.com>
2020-05-26 11:28:46 +0900

    12章

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ end
 
  get :favorites, to: 'favorites#index'
  post   "favorites/:dish_id/create"  => "favorites#create"
-  delete "favorites/:dish_id/destroy" => "favorites#destroy"
+delete "favorites/:dish_id/destroy" => "favorites#destroy"
+resources :comments, only: [:create, :destroy]
 end
